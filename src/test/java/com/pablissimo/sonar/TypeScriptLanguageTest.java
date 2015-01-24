5a40e993e2f9f3c89f938449f7efeb9d61834111
Pablissimo <turnacre@hotmail.com>
2015-01-24 21:44:42 +0000

    Testing TypeScriptLanguage

---
+++ src/test/java/com/pablissimo/sonar/TypeScriptLanguageTest.java
@@ -0,0 +1,34 @@
+package com.pablissimo.sonar;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TypeScriptLanguageTest {
+	TypeScriptLanguage language;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.language = new TypeScriptLanguage();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void identifiesCorrectly() {
+		assertEquals("TypeScript", this.language.getName());
+		assertEquals("ts", this.language.getKey());
+	}
+
+	@Test
+	public void definesCorrectExtension() {
+		String[] suffices = this.language.getFileSuffixes();
+		
+		assertEquals(1, suffices.length);
+		assertEquals("ts", suffices[0]);
+	}
+}
