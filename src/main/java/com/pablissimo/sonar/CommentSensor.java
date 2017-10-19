7d1c24822fc9181607b22ec3c15fb72728bcdf73
Simo Kutlin <simo.kutlin@gmail.com>
2017-10-19 15:10:15 +0200

    Adding comment lines and comment density metrics.

---
+++ src/main/java/com/pablissimo/sonar/CommentSensor.java
@@ -0,0 +1,14 @@
+package com.pablissimo.sonar;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.sonar.api.batch.BatchSide;
+import org.sonar.api.batch.fs.InputFile;
+import org.sonar.api.batch.sensor.SensorContext;
+
+@BatchSide
+@FunctionalInterface
+public interface CommentSensor {
+    Map<InputFile, Set<Integer>> execute(SensorContext ctx);
+}
--- src/test/java/com/pablissimo/sonar/CombinedCoverageSensorTest.java
+++ src/test/java/com/pablissimo/sonar/CombinedCoverageSensorTest.java
@@ -16,54 +16,63 @@ import org.sonar.api.batch.sensor.internal.SensorContextTester;
 
 public class CombinedCoverageSensorTest {
     LOCSensor locSensor;
+	CommentSensor commentSensor;
     TsCoverageSensor coverageSensor;
-    
+
     SensorContextTester sensorContext;
-    
+
     CombinedCoverageSensor sensor;
-    
+
     @Before
     public void setUp() throws Exception {
         this.sensorContext = SensorContextTester.create(new File(""));
         this.locSensor = mock(LOCSensor.class);
         this.coverageSensor = mock(TsCoverageSensor.class);
-        
-        this.sensor = new CombinedCoverageSensor(this.locSensor, this.coverageSensor);
+		this.commentSensor = mock(CommentSensor.class);
+
+        this.sensor = new CombinedCoverageSensor(this.locSensor, this.commentSensor, this.coverageSensor);
     }
-    
+
     @Test
     public void describe_setsName() {
         DefaultSensorDescriptor desc = new DefaultSensorDescriptor();
-        
+
         this.sensor.describe(desc);
-        
-        assertEquals(desc.name(), "Combined LCOV and LOC sensor");
+
+        assertEquals(desc.name(), "Combined LCOV, LOC and comments");
     }
-    
+
     @Test
     public void describe_setsLanguage() {
         DefaultSensorDescriptor desc = new DefaultSensorDescriptor();
-        
+
         this.sensor.describe(desc);
-        
+
         assertEquals(TypeScriptLanguage.LANGUAGE_KEY, desc.languages().iterator().next());
     }
-    
+
     @Test
     public void execute_callsLocSensor() {
         this.sensor.execute(this.sensorContext);
-        
+
         verify(this.locSensor, times(1)).execute(eq(this.sensorContext));
     }
-    
+
+    @Test
+    public void execute_callsCommentSensor() {
+        this.sensor.execute(this.sensorContext);
+
+        verify(this.commentSensor, times(1)).execute(eq(this.sensorContext));
+    }
+
     @Test
     public void execute_callsCoverageSensorWithLocSensorOutput() {
         Map<InputFile, Set<Integer>> locOutput = new HashMap<InputFile, Set<Integer>>();
-        
+
         when(this.locSensor.execute(eq(this.sensorContext))).thenReturn(locOutput);
-        
+
         this.sensor.execute(this.sensorContext);
-        
-        verify(this.coverageSensor, times(1)).execute(eq(this.sensorContext), eq(locOutput)); 
+
+        verify(this.coverageSensor, times(1)).execute(eq(this.sensorContext), eq(locOutput));
     }
 }
